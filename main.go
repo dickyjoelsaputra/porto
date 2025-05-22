f708a54f864d37b2451bf6d1b50c685fff9b946f
dickyjoelsaputra <dickyjoelsaputra@gmail.com>
2025-05-22 09:20:48 +0000

    push

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"html/template"
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -9,18 +11,38 @@ import (
 func main() {
 	e := echo.New()
 
-	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, `
-            <!DOCTYPE html>
-            <html>
-            <head><title>Hello Echo</title></head>
-            <body>
-                <h1>Hello from Echo + Docker!</h1>
-				<p>ngentod</p>
-            </body>
-            </html>
-        `)
-	})
+	// Serve static files from js and css directories
+	e.Static("/js", "js")
+	e.Static("/css", "css")
+	e.Static("/images", "images")
+
+	renderer := &TemplateRenderer{
+		templates: template.Must(template.ParseGlob("*.html")),
+	}
+
+	e.Renderer = renderer
+
+	e.GET("", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "index.html", map[string]interface{}{
+			"name": "Dolly!",
+		})
+	}).Name = "index"
 
 	e.Logger.Fatal(e.Start(":8080"))
+}
+
+// TemplateRenderer is a custom html/template renderer for Echo framework
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+// Render renders a template document
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+
+	// Add global methods if data is a map
+	if viewContext, isMap := data.(map[string]interface{}); isMap {
+		viewContext["reverse"] = c.Echo().Reverse
+	}
+
+	return t.templates.ExecuteTemplate(w, name, data)
 }
