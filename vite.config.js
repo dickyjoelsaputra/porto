bc4d84612dbdfcf57706e3fc8e398753175c6f52
dickyjoelsaputra <dickyjoelsaputra@gmail.com>
2025-07-04 13:25:55 +0700

    initial

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+import fullReload from 'vite-plugin-full-reload'
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+        // fullReload(['resources/views/**/*.blade.php'])
+    ],
+});
