9782e3e06ae64abd92e89eddd672d7d682df6fbe
dickyjoelsaputra <dickyjoelsaputra@gmail.com>
2025-07-05 10:53:41 +0700

    s

--- core/settings/production.py
+++ core/settings/production.py
@@ -19,7 +19,11 @@ MEDIA_URL = f"{AWS_S3_CUSTOM_DOMAIN}/media/"
 SECRET_KEY = "django-insecure-yc@op5ovy^^2ehcd48pk1(zpkw60z8b^ainnwuo*-q)g12ssk$"
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ["103.197.188.89","dickyjoel.icu","https://dickyjoel.icu"]
+ALLOWED_HOSTS = [
+    "103.197.188.89",
+    "dickyjoel.icu",
+    "https://dickyjoel.icu",
+]
 
 try:
     from .local import *
