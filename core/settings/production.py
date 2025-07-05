e6593d3f9e33628dc9ff366a5abb99f5b50504fa
dickyjoelsaputra <dickyjoelsaputra@gmail.com>
2025-07-05 10:50:35 +0700

    adding allowed host

--- core/settings/production.py
+++ core/settings/production.py
@@ -15,6 +15,12 @@ STORAGES = {
 STATIC_URL = f"{AWS_S3_CUSTOM_DOMAIN}/static/"
 MEDIA_URL = f"{AWS_S3_CUSTOM_DOMAIN}/media/"
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-yc@op5ovy^^2ehcd48pk1(zpkw60z8b^ainnwuo*-q)g12ssk$"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["103.197.188.89","dickyjoel.icu","https://dickyjoel.icu"]
+
 try:
     from .local import *
 except ImportError:
